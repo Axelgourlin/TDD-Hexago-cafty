81e4b5db6ed0516d50d9fb4d7875ae00c08ef8e6
AxelGourlin <gourlin.axel@gmail.com>
2024-02-22 18:18:24 -0500

    Add integration test for message prisma repo with test container

---
+++ src/infra/follow.prisma.repository.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+import { Follow, FollowsRepository } from "./../application/follows.repository";
+
+export class PrismaFollowsRepository implements FollowsRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async saveFollow(follow: Follow): Promise<void> {
+    await this.upsertUser(follow.user);
+    await this.upsertUser(follow.follow);
+    await this.prisma.user.update({
+      where: { name: follow.user },
+      data: {
+        following: {
+          connectOrCreate: [
+            {
+              where: { name: follow.follow },
+              create: { name: follow.follow },
+            },
+          ],
+        },
+      },
+    });
+  }
+  async getFollowsOf(user: string): Promise<string[]> {
+    const theUser = await this.prisma.user.findFirstOrThrow({
+      where: { name: user },
+      include: {
+        following: true,
+      },
+    });
+
+    return theUser.following.map((f) => f.name);
+  }
+
+  private async upsertUser(user: string) {
+    await this.prisma.user.upsert({
+      where: { name: user },
+      update: { name: user },
+      create: {
+        name: user,
+      },
+    });
+  }
+}
---
+++ src/infra/tests/message.prisma.repository.integration.test.ts
@@ -0,0 +1,160 @@
+import { messageBuilder } from "./../../tests/message.builder";
+import { PrismaClient } from "@prisma/client";
+import { exec } from "child_process";
+import { promisify } from "util";
+import { PostgreSqlContainer, StartedPostgreSqlContainer } from "@testcontainers/postgresql";
+import { PrismaMessageRepository } from "../message.prisma.repository";
+
+const asyncExec = promisify(exec);
+
+describe("PrismaMessageRepository", () => {
+  let container: StartedPostgreSqlContainer;
+  let prismaClient: PrismaClient;
+
+  beforeAll(async () => {
+    container = await new PostgreSqlContainer()
+      .withDatabase("crafty-test")
+      .withUsername("crafty-test")
+      .withPassword("crafty-test")
+      .withExposedPorts(5432)
+      .start();
+
+    const dataBaseUrl = `postgresql://${container.getUsername()}:${container.getPassword()}@${container.getHost()}:${container.getPort()}/${container.getDatabase()}?schema=public`;
+
+    prismaClient = new PrismaClient({
+      datasourceUrl: dataBaseUrl,
+    });
+
+    await asyncExec(`DATABASE_URL=${dataBaseUrl} npx prisma migrate deploy`);
+
+    return prismaClient.$connect();
+  }, 10000);
+
+  afterAll(async () => {
+    await container.stop({ timeout: 1000 });
+    return prismaClient.$disconnect();
+  });
+
+  beforeEach(async () => {
+    await prismaClient.message.deleteMany();
+    await prismaClient.$executeRawUnsafe(`DELETE FROM "User" CASCADE`);
+  });
+
+  test("save() should save a new message", async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const messageId = "message-id-1";
+    const messageAuthor = "Alice";
+    const messageText = "Hello World";
+    const messagePublishedAt = new Date("2023-01-19T19:00:00.000Z");
+
+    const message = messageBuilder()
+      .withId(messageId)
+      .withAuthor(messageAuthor)
+      .withText(messageText)
+      .withPublishedAt(messagePublishedAt)
+      .build();
+
+    await messageRepository.save(message);
+
+    const expectedMessage = await prismaClient.message.findUnique({
+      where: {
+        id: messageId,
+      },
+    });
+
+    expect(expectedMessage).toEqual({
+      id: messageId,
+      authorId: messageAuthor,
+      text: messageText,
+      publishedAt: messagePublishedAt,
+    });
+  });
+
+  test("save() should save an existing message", async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const messageId = "message-id-1";
+    const messageAuthor = "Alice";
+    const messageText = "Hello World";
+    const messageEditedText = "Edited message";
+    const messagePublishedAt = new Date("2023-01-19T19:00:00.000Z");
+
+    const aliceMessageBuilder = messageBuilder()
+      .withId(messageId)
+      .withAuthor(messageAuthor)
+      .withText(messageText)
+      .withPublishedAt(messagePublishedAt);
+
+    await messageRepository.save(aliceMessageBuilder.build());
+
+    await messageRepository.save(aliceMessageBuilder.withText(messageEditedText).build());
+
+    const expectedMessage = await prismaClient.message.findUnique({
+      where: {
+        id: messageId,
+      },
+    });
+
+    expect(expectedMessage).toEqual({
+      id: messageId,
+      authorId: messageAuthor,
+      text: messageEditedText,
+      publishedAt: messagePublishedAt,
+    });
+  });
+
+  test("getById() should get a message by its id", async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const messageId = "message-id-1";
+
+    const aliceMessage = messageBuilder()
+      .withId(messageId)
+      .withAuthor("Alice")
+      .withText("Hello World")
+      .withPublishedAt(new Date("2023-01-19T19:00:00.000Z"))
+      .build();
+
+    await messageRepository.save(aliceMessage);
+
+    const expectedMessage = await messageRepository.getById(messageId);
+
+    expect(expectedMessage).toEqual(aliceMessage);
+  });
+
+  test("getAllOfUser() should get all user messages", async () => {
+    const messageRepository = new PrismaMessageRepository(prismaClient);
+
+    const messageId = "message-id-1";
+    const messageId2 = "message-id-2";
+    const messageId3 = "message-id-3";
+
+    const aliceMessage = messageBuilder()
+      .withId(messageId)
+      .withAuthor("Alice")
+      .withText("Alice message 1")
+      .withPublishedAt(new Date("2023-01-19T19:00:00.000Z"))
+      .build();
+
+    const aliceMessage2 = messageBuilder()
+      .withId(messageId2)
+      .withAuthor("Alice")
+      .withText("Alice message 2")
+      .withPublishedAt(new Date("2023-01-19T19:00:00.000Z"))
+      .build();
+
+    const bobMessage = messageBuilder()
+      .withId(messageId3)
+      .withAuthor("Bob")
+      .withText("Bob message 1")
+      .withPublishedAt(new Date("2023-01-19T19:00:00.000Z"))
+      .build();
+
+    await Promise.all([aliceMessage, aliceMessage2, bobMessage].map((message) => messageRepository.save(message)));
+
+    const expectedMessages = await messageRepository.getAllByAuthor("Alice");
+    expect(expectedMessages).toHaveLength(2);
+    expect(expectedMessages).toEqual(expect.arrayContaining([aliceMessage, aliceMessage2]));
+  });
+});
